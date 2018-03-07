22e9237bbd11b331d184b1c4426db8962fa75d00
lzamboni <lzamboni@brrjobitanl1098.la.ad.b2w>
2018-03-07 19:21:09 -0300

    ADD search feed

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
+"use strict"
 
 const app = require("express")()
-
+const moment = require("moment")
 
 const mongoose = require("./initializers/mongoose")()
 
@@ -10,6 +11,7 @@ const Schema = mongoose.Schema;
 const eventSchema = new Schema({
   type:  { type: String, required: true },
   payload: { type: Object, required: true },
+  date: { type: Date, default: Date.now },
   meta: {
     origin: String,
     referer:  String
@@ -47,8 +49,32 @@ app.put("/api/events/register", (req, res) => {
 })
 
 app.get("/api/events/feed/:time/:period/client/:clienId", (req, res) => {
-  res.json({ acknowledge: true })
-  
+  const { time, period, clienId } = req.params
+
+  let startDate, endDate
+
+  switch(time) {
+    case "hour":
+
+    case "day":
+      startDate = moment(period).startOf("day")
+      endDate =  moment(period).endOf("day")
+    case "month":
+  }
+
+  Event.find({
+    date: {
+      $gte: startDate.toDate(),
+      $lt: endDate.toDate()
+    },
+  }).then(events => {
+    res.json({
+      events,
+      links: [
+        { rel: "self" }
+      ]
+    })
+  })
 })
 
 app.put("/api/events/:eventId/ok/:clientId", (req, res) => {
